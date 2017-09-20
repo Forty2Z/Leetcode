78b3772a78f3c905c2494e4e8249a595c477cac5
Mengran Zhu <zzmmrr0504@gmail.com>
2017-09-19 20:46:13 -0700

    create lc 496

---
+++ src/com/forty2leetcode/stack/NextGreaterElement.java
@@ -0,0 +1,15 @@
+package com.forty2leetcode.stack;
+
+
+/*
+*Leetcode : 496
+*author: Forty2
+* date:09/19/2017
+*
+*
+* */
+public class NextGreaterElement {
+public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+    return null;
+}
+}
